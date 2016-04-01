66b26653815e1a676eabe96d10ad64c8ae35fafb
sid <oceankwok@hotmail.com>
2016-04-01 21:51:15 +0800

    add writeCss()

--- createcss.js
+++ createcss.js
@@ -1,5 +1,27 @@
 var fs = require('fs');
 
+function writeCss(w){
+  var year, month, day;
+  var css = "";
+  w.forEach(function(item){
+    year  = parseInt(w.date.split('-')[0]);
+    month = parseInt(w.date.split('-')[1]);
+    day   = parseInt(w.date.split('-')[2]);
+
+    css += "body > table > tbody > tr:nth-child(" +
+           (13 - month) +
+           ") > td:nth-child(" +
+           (1 + day) + ")" +
+           "{background: red}/n";
+  });
+
+  fs.writeFile( __dirname + '/css/table.css', css, function(err){
+    if (err){
+      console.error(err);
+    }
+  });
+}
+
 fs.readdir(__dirname + '/post', function(err, files){
   if (err){
     console.error(err);
@@ -7,20 +29,33 @@ fs.readdir(__dirname + '/post', function(err, files){
 
   // file size and path, and sorry that I cant get size right now
   var size, path, title, date;
-  var wrap;
+  var wrap = [];
   files.forEach(function(item){
     //sorry for mac
     if(item !== ".DS_Store"){
       path = __dirname + '/post/' + item;
-      // size = fs.statSync(path).size;
-      fs.readFile(path, "utf-8", function(err, content){
-        wrap = {
+      fs.stat(path, function(err, stats){
+        wrap.push({
           title: item.split("_")[0],
           date: item.split("_")[1],
-          content: content,
-          size: 200
-        };
+          size: stats.size,
+          path: path
+        });
+        // need to put wrap into writeCss when the fs.stat event is done
+        // console.log(wrap);
       });
+      // (function(s, p){
+      //   fs.readFile(path, "utf-8", function(err, content){
+      //     wrap = {
+      //       title: item.split("_")[0],
+      //       date: item.split("_")[1],
+      //       content: 0,
+      //       size: s,
+      //       path: p
+      //     };
+      //     console.log(wrap);
+      //   });
+      // })(size, path);
     }
   });
 });
