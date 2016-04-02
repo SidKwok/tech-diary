8d3683b4b80490f1b69b6640d165461034bf721a
sid <oceankwok@hotmail.com>
2016-04-02 11:23:21 +0800

    be able to write css based on posts

--- createcss.js
+++ createcss.js
@@ -3,16 +3,32 @@ var fs = require('fs');
 function writeCss(w){
   var year, month, day;
   var css = "";
+  var posts = {};
+  var date = [];
   w.forEach(function(item){
-    year  = parseInt(w.date.split('-')[0]);
-    month = parseInt(w.date.split('-')[1]);
-    day   = parseInt(w.date.split('-')[2]);
 
+    if(date.indexOf(item.date) == -1){
+      date.push(item.date);
+      posts[item.date] = {
+        title: new Array(item.title),
+        size: item.size
+      };
+    } else {
+      posts[item.date].title.push(item.title);
+      posts[item.date].size += item.size;
+    }
+
+  });
+
+  date.forEach(function(item){
+    year  = parseInt(item.split('-')[0]);
+    month = parseInt(item.split('-')[1]);
+    day   = parseInt(item.split('-')[2]);
     css += "body > table > tbody > tr:nth-child(" +
            (13 - month) +
            ") > td:nth-child(" +
            (1 + day) + ")" +
-           "{background: red}/n";
+           "{background: red}\n";
   });
 
   fs.writeFile( __dirname + '/css/table.css', css, function(err){
@@ -22,6 +38,8 @@ function writeCss(w){
   });
 }
 
+
+
 fs.readdir(__dirname + '/post', function(err, files){
   if (err){
     console.error(err);
@@ -30,6 +48,7 @@ fs.readdir(__dirname + '/post', function(err, files){
   // file size and path, and sorry that I cant get size right now
   var size, path, title, date;
   var wrap = [];
+  var flength = files.length;
   files.forEach(function(item){
     //sorry for mac
     if(item !== ".DS_Store"){
@@ -37,12 +56,14 @@ fs.readdir(__dirname + '/post', function(err, files){
       fs.stat(path, function(err, stats){
         wrap.push({
           title: item.split("_")[0],
-          date: item.split("_")[1],
+          date: item.split("_")[1].split(".")[0],
           size: stats.size,
           path: path
         });
         // need to put wrap into writeCss when the fs.stat event is done
-        // console.log(wrap);
+        if(wrap.length == flength){
+          writeCss(wrap);
+        }
       });
       // (function(s, p){
       //   fs.readFile(path, "utf-8", function(err, content){
@@ -56,6 +77,8 @@ fs.readdir(__dirname + '/post', function(err, files){
       //     console.log(wrap);
       //   });
       // })(size, path);
+    } else {
+      flength -= 1;
     }
   });
 });
