13a2149ca274ca48368e88f5afe21e41886b4187
sid <oceankwok@hotmail.com>
2016-04-03 20:26:54 +0800

    ud new post

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -87,7 +87,7 @@
 /* 1 */
 /***/ function(module, exports) {
 
-	function generateData(){return {"2016-03-29":{"title":["applyAndcall","jsChain"],"size":2668},"2016-03-30":{"title":["gulpAndBS"],"size":1951},"2016-04-01":{"title":["jsInherit","prototypeAndconstructor"],"size":4528},"2016-03-31":{"title":["jsModule"],"size":2337},"2016-04-02":{"title":["jsonpAndAjax"],"size":2655}};};module.exports=generateData;
+	function generateData(){return {"2016-04-03":{"title":["aboutWebpack"],"size":1623},"2016-03-29":{"title":["applyAndcall","jsChain"],"size":2668},"2016-03-30":{"title":["gulpAndBS"],"size":1951},"2016-04-01":{"title":["jsInherit","prototypeAndconstructor"],"size":4528},"2016-03-31":{"title":["jsModule"],"size":2337},"2016-04-02":{"title":["jsonpAndAjax"],"size":2655}};};module.exports=generateData;
 
 /***/ },
 /* 2 */
