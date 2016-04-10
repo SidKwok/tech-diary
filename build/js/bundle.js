c7bb7dd45df6d8b409bb2580229b91c84410e139
sid <oceankwok@hotmail.com>
2016-04-10 15:43:45 +0800

    pretify style

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -111,7 +111,7 @@
 /* 1 */
 /***/ function(module, exports) {
 
-	function generateData(){return {"2016-04-03":{"title":["aboutWebpack"],"size":1643},"2016-03-29":{"title":["applyAndcall","jsChain"],"size":2702},"2016-04-06":{"title":["crossBrowser","thisAndClosure"],"size":8028},"2016-04-05":{"title":["cssFlex"],"size":3524},"2016-04-08":{"title":["currying"],"size":2219},"2016-04-07":{"title":["fasterWebSites"],"size":2544},"2016-04-10":{"title":["gulp"],"size":5821},"2016-03-30":{"title":["gulpAndBS"],"size":1963},"2016-04-01":{"title":["jsInherit","prototypeAndconstructor"],"size":4601},"2016-03-31":{"title":["jsModule"],"size":2404},"2016-04-02":{"title":["jsonpAndAjax"],"size":2713},"2016-04-04":{"title":["webStorage"],"size":3810}};};module.exports=generateData;
+	function generateData(){return {"2016-04-03":{"title":["aboutWebpack"],"size":1643},"2016-04-06":{"title":["crossBrowser","thisAndClosure"],"size":8028},"2016-03-29":{"title":["applyAndcall","jsChain"],"size":2702},"2016-04-05":{"title":["cssFlex"],"size":3524},"2016-04-08":{"title":["currying"],"size":2219},"2016-04-07":{"title":["fasterWebSites"],"size":2544},"2016-03-30":{"title":["gulpAndBS"],"size":1963},"2016-04-10":{"title":["gulp"],"size":5821},"2016-04-01":{"title":["jsInherit","prototypeAndconstructor"],"size":4601},"2016-03-31":{"title":["jsModule"],"size":2404},"2016-04-02":{"title":["jsonpAndAjax"],"size":2713},"2016-04-04":{"title":["webStorage"],"size":3810}};};module.exports=generateData;
 
 /***/ },
 /* 2 */
