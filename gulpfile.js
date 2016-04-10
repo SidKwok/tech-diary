08fc9239d52578028969d6d8e869eab6e0a6459a
sid <oceankwok@hotmail.com>
2016-04-10 15:20:45 +0800

    add new post

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,43 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var jade        = require('gulp-jade');
-var webpack     = require('webpack-stream');
-var markdown    = require('gulp-markdown');
+var jade = require('gulp-jade');
+var webpack = require('webpack-stream');
+var markdown = require('gulp-markdown');
 var frontMatter = require('gulp-front-matter');
-var layout      = require('gulp-layout');
-var createcss   = require('./app/createcss');
-var ghPages     = require('gulp-gh-pages');
-var reload      = browserSync.reload;
+var layout = require('gulp-layout');
+var createcss = require('./app/createcss');
+var ghPages = require('gulp-gh-pages');
+var reload = browserSync.reload;
 
 gulp.task('serve', ['jade'], function() {
 
-    browserSync.init({
-        server: "./build/"
-    });
+  browserSync.init({
+    server: "./build/"
+  });
 
-    gulp.watch("app/*.js", ["webpack"]);
-    gulp.watch("app/jade/*.jade", ['jade']);
-    gulp.watch(["./build/*.html", "./build/css/*.css", "./build/js/*.js"]).on('change', reload);
+  gulp.watch("app/*.js", ["webpack"]);
+  gulp.watch("app/jade/*.jade", ['jade']);
+  gulp.watch(["./build/*.html", "./build/css/*.css", "./build/js/*.js"]).on('change', reload);
 });
 
 gulp.task('jade', function() {
-    return gulp.src("app/jade/index.jade")
-        .pipe(jade({
-          pretty: true
-        }))
-        .pipe(gulp.dest("./build/"))
-        .pipe(reload({stream: true}));
+  return gulp.src("app/jade/index.jade")
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest("./build/"))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
-gulp.task('webpack', function(){
+gulp.task('webpack', function() {
   return gulp.src("app/index.js")
-      .pipe(webpack(require("./webpack.config.js")))
-      .pipe(gulp.dest("./build/js/"))
-      .pipe(reload({stream: true}));
+    .pipe(webpack(require("./webpack.config.js")))
+    .pipe(gulp.dest("./build/js/"))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 gulp.task('posts', function() {
